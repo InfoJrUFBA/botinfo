155b3324c9417875f53e27d417bd59dfab0bf304
thebestlucc <thebestlucc@gmail.com>
2020-04-08 20:41:14 -0300

    First commit

---
+++ src/commands/ivensTroll.js
@@ -0,0 +1,14 @@
+module.exports = {
+	async run(client, message) {
+        
+    },
+
+	get command() {
+		return {
+			name: 'troll',
+			category: categories.USER,
+			description: 'Descrição do Comando',
+			usage: 'comando',
+		};
+	},
+};
---
+++ src/events/message.js
@@ -0,0 +1,47 @@
+
+const runCommand = async (client, message) => {
+	const args = message.content
+		.slice(process.env.COMMAND_PREFIX.length)
+		.trim()
+        .split(/ +/g);
+
+	const command = args.shift().toLowerCase();
+
+	const cmd = client.commands.get(command);
+	if (!cmd) return;
+
+	message.delete().catch(() => {});
+
+	console.log(
+		'[#LOG]',
+		`${message.author.username} (${
+			message.author.id
+		}) executou o comando: ${cmd.command.name}`
+	);
+	try {
+		if (cmd.validate) {
+			await cmd.validate(client, message, args);
+		}
+		await cmd.run(client, message, args);
+		if (cmd.success) {
+			await cmd.success(client, message, args);
+		}
+	} catch (err) {
+		console.error(err);
+		if (cmd.fail) {
+			await cmd.fail(err, client, message, args);
+			return;
+		}
+	} finally {
+		if (cmd.after) {
+			await cmd.after(client, message, args);
+		}
+	}
+};
+
+
+module.exports = async (client, message) => {
+    await Promise.all([
+		runCommand(client, message),
+	]);
+}
