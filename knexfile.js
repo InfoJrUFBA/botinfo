a3fb2ccc5258012502cda6e1854bb1af9d219b4a
Ivens <ivensjorisnbacon@gmail.com>
2020-04-25 12:55:35 -0300

    initial configs for database with knexjs

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+module.exports = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    ssl: true
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    directory: './src/database/migrations',
+    tableName: 'knex_migrations'
+  }
+};
