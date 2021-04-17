81a06aea084598dbb9e172ce956cef02fa5274b4
Ivens <ivensjorisnbacon@gmail.com>
2021-04-17 15:39:20 -0300

    test on heroku

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,26 +1,45 @@
-import { User, MeetPresence } from '../database/models'
-import { isWithinInterval } from 'date-fns'
+/* eslint-disable no-unused-vars */
+// import { User, MeetPresence } from '../database/models'
+// import { isWithinInterval } from 'date-fns'
+// import { Client } from 'discord.js'
 
+import { Channel, Client, VoiceState } from 'discord.js'
+
+/**
+ *
+ * @param {Client} client
+ * @param {VoiceState} oldState
+ * @param {VoiceState} newState
+ * @returns
+ */
 module.exports = async (client, oldState, newState) => {
   const discordUser = oldState?.member?.user || newState?.member?.user
-  const discordChannel = oldState?.channel || newState?.channel
+  const channel = client.channels.cache.get('824443233983397899')
+
   if (discordUser.bot) return
 
-  const userFromDb = await User.findOneAndUpdate({
-    discord_id: discordUser.id
-  }, { name: discordUser.username }, { upsert: true })
+  if (oldState?.channel) {
+    channel.send(`Usuario \`${discordUser.username}\` saiu no canal ${oldState.channel}`)
+  }
 
-  const now = new Date()
-
-  const meet = await MeetPresence.findOne({
-    startTime: { $lt: now },
-    endTime: { $gt: now },
-    'voice_channel.id': discordChannel.id
-  }).populate('owner')
-
-  if (meet && isWithinInterval(now, { start: meet.startTime, end: meet.endTime })) {
-    try {
-      await MeetPresence.updateOne({ _id: meet._id }, { $addToSet: { participants: userFromDb._id } })
-    } catch {}
+  if (newState?.channel) {
+    channel.send(`Usuario \`${discordUser.username}\` entrou no canal ${newState.channel}`)
   }
 }
+// const userFromDb = await User.findOneAndUpdate({
+//   discord_id: discordUser.id
+// }, { name: discordUser.username }, { upsert: true })
+
+// const now = new Date()
+
+// const meet = await MeetPresence.findOne({
+//   startTime: { $lt: now },
+//   endTime: { $gt: now },
+//   'voice_channel.id': discordChannel.id
+// }).populate('owner')
+
+// if (meet && isWithinInterval(now, { start: meet.startTime, end: meet.endTime })) {
+//   try {
+//     await MeetPresence.updateOne({ _id: meet._id }, { $addToSet: { participants: userFromDb._id } })
+//   } catch {}
+// }
